dbf933cf0fe7c302e814aac4f4853779482d3e9b
Jaime Barriga <JB4GDI@gmail.com>
2017-09-13 19:02:06 +0000

    Changed the user model so it automatically creates a master ledger attached to the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,19 @@
 class User < ApplicationRecord
+
+  # On user creation, we are going to automatically create a ledger in the database just for them
+  after_create :create_master_ledger_for_user
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :ledgers
+
+  private
+
+  # Create a ledger in the database that's tied to the current user.  This will be their master list.
+  def create_master_ledger_for_user
+    Ledger.create(title: 'My List', user_id: self.id)
+  end
 end
